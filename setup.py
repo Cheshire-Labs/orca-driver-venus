5fbba49ddab69d0ed5ea7967b6d45d2d7c2c3491
Michael Salmi <miikee713@gmail.com>
2024-09-22 23:48:55 -0400

    Adds setup.py, license, and dev-requirements.txt

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+# Function to read requirements.txt
+def read_requirements():
+    with open('requirements.txt') as req_file:
+        return req_file.read().splitlines()
+
+setup(
+    name='venus-orca-driver',
+    version='0.1.0', 
+    description='Hamilton Venus command line driver',
+    author='Cheshire Labs',
+    author_email='michaeltsalmi@cheshirelabs.com',
+    url='https://github.com/Cheshire-Labs/venus-driver',
+    packages=find_packages(include=['venus_driver', 'venus_driver.*']),
+    install_requires=read_requirements(),
+    classifiers=[
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: GNU Affero General Public License v3 (AGPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10'
+)
